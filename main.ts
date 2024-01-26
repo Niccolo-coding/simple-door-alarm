aeb3b95bf480669c262d6fa64fb99ff08c0c759f
Niccolo-coding <156704626+Niccolo-coding@users.noreply.github.com>
2024-01-26 20:05:04 +0100

    Aggiorna pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
+input.onPinReleased(TouchPin.P0, function () {
+	
+})
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.magneticForce(Dimension.Strength))
+	
 })
 basic.forever(function () {
-    if (input.magneticForce(Dimension.Strength) < 200) {
+    if (input.magneticForce(Dimension.Strength) > 200) {
         basic.showIcon(IconNames.Angry)
+        JoyPiAdvanced.oledShowString("Ciao")
     }
 })
